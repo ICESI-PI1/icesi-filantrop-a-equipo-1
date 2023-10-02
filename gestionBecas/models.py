59bd567a6765ad7a4ce92b54f2ecbee18e57b48c
Manuel Herrera <mahupa25@gmail.com>
2023-10-02 14:23:13 -0500

    feat:Asignar_Rol y Vistas Rol
    Se añadio la funcion de roles y que muestre una vista segun rol

--- gestionBecas/models.py
+++ gestionBecas/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def asignar_rol_sin_rol(sender, instance, created, **kwargs):
+    if created:
+        instance.first_name = "Sin Rol"
+        instance.save()
+
 
 class Rol(models.Model):
     nombre = models.CharField(max_length=100)
