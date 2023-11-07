5fe44b99fd2cfebfb50f38f2e2cdca063fa3c3ce
Juan181803 <jframirez.valencia@gmail.com>
2023-11-07 02:28:49 -0500

    feat_ver_cronograma

--- gestionBecas/views/ver_cronograma.py
+++ gestionBecas/views/ver_cronograma.py
@@ -1,13 +1,16 @@
-# views.py
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from gestionBecas.models import Cronograma
 
-class VerCronogramaView(View):
-    template_name = 'ver_cronograma.html'  
 
+
+class VerCronogramaView(View):
     def get(self, request):
         cronogramas = Cronograma.objects.all()
-        context = {'cronogramas': cronogramas}
-        return render(request, self.template_name, context)
+        return render(request, 'ver_cronograma.html', {'cronogramas': cronogramas})
+
+    def post(self, request):
+        cronograma_id = request.POST.get('nombre')
+        cronograma_seleccionado = get_object_or_404(Cronograma, id=cronograma_id)
+        cronogramas = Cronograma.objects.all()  # Agrega esta línea para obtener la lista de cronogramas
+        return render(request, 'ver_cronograma.html', {'cronogramas': cronogramas, 'cronograma_seleccionado': cronograma_seleccionado})
