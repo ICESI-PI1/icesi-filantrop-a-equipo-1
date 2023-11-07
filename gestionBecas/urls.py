597ac44f7d3cc5be9c5b1bf26ff949deec7f96c8
CrisDelCast <rocosa2003@gmail.com>
2023-11-06 18:15:44 -0600

    feat-cronograma: funcion de creacion del cronograma terminada

--- gestionBecas/urls.py
+++ gestionBecas/urls.py
@@ -20,6 +20,7 @@ from .views.editar_cronograma_view import EditarCronogramaView
 from .views.registrar_programa_beca_view import RegistrarProgramaBecaView
 from .views.eliminar_usuario_view import EliminarUsuarioView
 from .views.editar_roll_view import EditarRollNameView
+from .views.seleccionar_pb2 import Seleccionar2ProgramaBecaView
 
 app_name = 'app_login'
 
@@ -35,14 +36,14 @@ urlpatterns  = [
     path('inicio_sin_rol/', views.inicio_sin_rol_view.InicioSinRolView.as_view(), name='inicio_sin_rol'),
     path('inicio_donante/', views.inicio_donante_view.InicioDonanteView.as_view(), name='inicio_donante'),
     path('ver_programa_beca/', views.ver_programa_beca_view.VerProgramaBecaView.as_view(), name='ver_programa_beca'),
+    path('seleccionar_pb2/', views.seleccionar_pb2.Seleccionar2ProgramaBecaView.as_view(), name='seleccionar_pb2'),
     path('seleccionar_programa_beca/', views.seleccionar_programa_beca_view.SeleccionarProgramaBecaView.as_view(), name='seleccionar_programa_beca'),
     path('editar_programa_beca/<int:programa_id>/', views.editar_programa_beca_view.EditarProgramaBecaView.as_view(), name='editar_programa_beca'),
-    path('registrar_cronograma/', views.registrar_cronograma_view.CrearCronogramaView.as_view(), name='registrar_cronograma'),
+    path('registrar_cronograma/<int:programa_id>/', views.registrar_cronograma_view.CrearCronogramaView.as_view(), name='registrar_cronograma'),
     path('gestion_informe_beca/', views.gestion_informe_beca_view.GestionInformeBecaView.as_view(), name='gestion_informe_beca'),
     path('seleccionar_cronograma/', views.seleccionar_cronograma_view.SeleccionarCronogramaView.as_view(), name='seleccionar_cronograma'),
     path('editar_cronograma/<int:cronograma_id>/', views.editar_cronograma_view.EditarCronogramaView.as_view(), name='editar_cronograma'),
     path('registrar_programa_beca/', views.registrar_programa_beca_view.RegistrarProgramaBecaView.as_view(), name='registrar_programa_beca'),
     path('eliminar_usuario/<str:username>/', views.eliminar_usuario_view.EliminarUsuarioView.as_view(), name='eliminar_usuario'),
     path('editar_roll/<str:username>/', views.editar_roll_view.EditarRollNameView.as_view(), name='editar_rollname'),
-
 ]
--- gestionBecas/views/registrar_cronograma_view.py
+++ gestionBecas/views/registrar_cronograma_view.py
@@ -1,33 +1,21 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import View
-from django.shortcuts import render, redirect
-from gestionBecas.models import ProgramaBeca, TipoConvocatoria, Cronograma
+from django.shortcuts import get_object_or_404, render, redirect
+from gestionBecas.models import ProgramaBeca
 from gestionBecas.forms import CronogramaForm
 
 class CrearCronogramaView(View):
-    def get(self, request):
-        programas_beca = ProgramaBeca.objects.all()
-        tipos_convocatoria = TipoConvocatoria.objects.all()
-        return render(request, 'registrar_cronograma.html', {'programas_beca': programas_beca, 'tipos_convocatoria': tipos_convocatoria})
+    def get(self, request, programa_id):
+        programa_beca = get_object_or_404(ProgramaBeca, id=programa_id)
+        form = CronogramaForm(initial={'programa_becas': programa_beca})
+        return render(request, 'registrar_cronograma.html', {'form': form})
 
-    def post(self, request):
-        programa_becas = request.POST['programa_becas']
-        tipo_convocatoria = request.POST['tipo_convocatoria']
-        fecha_inscripciones = request.POST['fecha_inscripciones']
-        fecha_cierre_inscripciones = request.POST['fecha_cierre_inscripciones']
-        fecha_seleccion_aspirantes = request.POST['fecha_seleccion_aspirantes']
-        fecha_entrevistas = request.POST['fecha_entrevistas']
-        fecha_publicacion_beneficiarios = request.POST['fecha_publicacion_beneficiarios']
-
-        programa_beca_obj = ProgramaBeca.objects.create(nombre=programa_becas)
-        tipo_convocatoria_obj = TipoConvocatoria.objects.create(nombre=tipo_convocatoria)
-        cronograma_obj = Cronograma.objects.create(
-            programa_becas=programa_beca_obj,
-            tipo_convocatoria=tipo_convocatoria_obj,
-            fecha_inscripciones=fecha_inscripciones,
-            fecha_cierre_inscripciones=fecha_cierre_inscripciones,
-            fecha_seleccion_aspirantes=fecha_seleccion_aspirantes,
-            fecha_entrevistas=fecha_entrevistas,
-            fecha_publicacion_beneficiarios=fecha_publicacion_beneficiarios
-        )
-
-        return render(request, 'inicio.html')
+    def post(self, request, programa_id):
+        form = CronogramaForm(request.POST)
+        if form.is_valid():
+            cronograma_obj = form.save(commit=False)
+            cronograma_obj.programa_becas = get_object_or_404(ProgramaBeca, id=programa_id)
+            cronograma_obj.save()
+            return HttpResponseRedirect(reverse('app_login:inicio'))  # Redirige a la página de éxito después de crear el cronograma
+        return render(request, 'registrar_cronograma.html', {'form': form})
