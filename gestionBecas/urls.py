654f6ad9b5d79f7a710eb8fa72a34fb284b00b96
Manuel Herrera <98495072+Herre1@users.noreply.github.com>
2023-11-14 20:03:46 -0500

    fix : Vista html
    Implementado en
    Login
    Inicio
    Ver programa beca
    Eliminar Programa beca

--- gestionBecas/urls.py
+++ gestionBecas/urls.py
@@ -12,7 +12,6 @@ urlpatterns  = [
   path('signup/', views.signup_view, name='signup'),
   path('perfil/', views.perfil, name='perfil'),
   path('', views.homepage, name='homepage'),
-  path('asignar_roles/', views.asignar_roles, name='asignar_roles'),
   path('gestion_programa_beca/', views.gestion_programa_beca, name='gestion_programa_beca'),
   path('lista_usuarios/', lista_usuarios, name='lista_usuarios'),
   path('editar_firstname/<str:username>/', views.editar_firstname, name='editar_firstname'),
